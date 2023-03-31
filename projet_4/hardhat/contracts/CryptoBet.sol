6108d9083c88280614e98191eac0b960fd63e2f2
Baptiste <avocalam@gmail.com>
2023-03-31 14:19:14 +0200

    create win los and nexRound function

--- projet_4/hardhat/contracts/CryptoBet.sol
+++ projet_4/hardhat/contracts/CryptoBet.sol
@@ -3,80 +3,135 @@ pragma solidity ^0.8.17;
 
 // import "hardhat/console.sol";
 import "../node_modules/@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 
-contract CryptoBet {
-    // --- VARIABLES ---
-    uint256 currentRound;
-    AggregatorV3Interface internal priceFeed;
+contract CryptoBet is Ownable {
+  // --- VARIABLES ---
+  struct UserBet {
+      uint256 bet_value;
+      uint256 balance;
+      Expectations expectStatus;
+      Results betStatus;
+      bool isBetting;
+  }
 
-    struct Bet {
-        uint256 bet_value;
-        uint256 start_crypto_value;
-        uint256 block_number;
-        bool expectation;
-        bool isBetting;
+  enum Expectations {
+    noting,
+    down,
+    up
+  }
+
+  enum Results {
+    noting,
+    loose,
+    win
+  }
+
+  mapping(address => UserBet) userBets;
+
+  address[] betters;
+  uint256 currentBetBalance;
+
+  int256 currentPriceFeed;
+  uint256 DEFAULT_BET_VALUE = 10;
+
+  AggregatorV3Interface internal priceFeed;
+
+  // --- EVENTS ---
+
+  // --- CONSTRUCTOR ---
+  
+  // sepolia testnet price feed address (ETH vs USD) :
+  // 0x694AA1769357215DE4FAC081bf1f309aDC325306
+
+  constructor() {
+      priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+  }
+
+  // --- MODIFIERS ---
+
+  modifier mustNotBeBetting() {
+      require(
+          userBets[msg.sender].isBetting == false,
+          unicode"must not betting to execute this function"
+      );
+      _;
+  }
+
+  modifier mustBeBetting() {
+      require(
+          userBets[msg.sender].isBetting == true,
+          unicode"must be betting to execute this function"
+      );
+      _;
+  }
+
+  // --- FUCTIONS ---
+
+  function nextRound() external {
+    Expectations answerStatus = Expectations(uint256(1));
+    int256 newPriceFeed = getLatestPrice();
+
+    uint256 winners;
+
+    if (newPriceFeed > currentPriceFeed) {
+      answerStatus = Expectations(uint256(2));
+    }
+    currentPriceFeed = newPriceFeed;
+
+    for (uint256 i = 0; i < betters.length; i = i + 1) {
+      if (userBets[betters[i]].expectStatus == answerStatus) {
+        userBets[betters[i]].betStatus == Results(uint256(2));
+        winners = winners + 1;
+        // EVT -> you win
+      } else {
+        userBets[betters[i]].betStatus == Results(uint256(1));
+        // EVT -> you loose
+      }
     }
 
-    struct BetRound{
-      address[] betters;
-      uint256 workflowBetBalance;
+    computeRewards(winners);
+    delete betters;
+    // EVT -> next round
+  }
+
+  function computeRewards(uint256 win) private {
+    for (uint256 i = 0; i < betters.length; i = i + 1) {
+      if (userBets[betters[i]].betStatus == Results(uint256(2))) {
+        userBets[betters[i]].balance = win / currentBetBalance;
+      }
+    }
+  }
+
+  // register a bet
+  function registerBet(uint256 _expectation) external payable mustNotBeBetting {
+    betters.push(msg.sender);
+    userBets[msg.sender].bet_value = DEFAULT_BET_VALUE;
+    userBets[msg.sender].expectStatus = Expectations(uint256(_expectation));
+    userBets[msg.sender].isBetting = true;
+  }
+
+  // claim a bet
+  function claimBet() external mustBeBetting {
+    if (userBets[msg.sender].betStatus == Results(uint256(2))) {
+      // VIREMENT userBets[msg.sender].balance
     }
 
-    mapping(address => uint256) balances;
-    mapping(address => Bet) bets;
+    delete userBets[msg.sender].bet_value;
+    delete userBets[msg.sender].expectStatus;
+    delete userBets[msg.sender].balance;
+    userBets[msg.sender].isBetting = false;
+  }
 
-    uint256 DEFAULT_BET_VALUE = 10 wei;
-
-    // --- EVENTS ---
-
-    // --- CONSTRUCTOR ---
-    
-    // sepolia testnet price feed address (ETH vs USD) :
-    // 0x694AA1769357215DE4FAC081bf1f309aDC325306
-
-    constructor() {
-        priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-    }
-
-    // --- MODIFIERS ---
-
-    modifier mustNotBeBetting() {
-        require(
-            bets[msg.sender].isBetting == false,
-            unicode"must not betting to execute this function"
-        );
-        _;
-    }
-
-    modifier mustBeBetting() {
-        require(
-            bets[msg.sender].isBetting == true,
-            unicode"must be betting to execute this function"
-        );
-        _;
-    }
-
-    // --- FUCTIONS ---
-
-    // get latest price
-    function getLatestPrice() public view returns (int) {
-    (
-        uint80 roundID, 
-        int price,
-        uint startedAt,
-        uint timeStamp,
-        uint80 answeredInRound
-    ) = priceFeed.latestRoundData();
-        return price;
-    }
-
-    // register a bet
-    function registerBet(bool _expectation) external payable mustNotBeBetting {
-      // do something
-    }
-
-    // claim a bet
-    function claimBet() external mustBeBetting {
-      // do someting
-    }
+  // get latest price
+  function getLatestPrice() public view returns (int) {
+  (
+      uint80 roundID, 
+      int price,
+      uint startedAt,
+      uint timeStamp,
+      uint80 answeredInRound
+  ) = priceFeed.latestRoundData();
+      return price;
+  }
 }
