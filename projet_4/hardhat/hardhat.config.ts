1717138975a37209a69dbac5c457edfabc2eb21a
Baptiste <avocalam@gmail.com>
2023-04-02 20:37:28 +0200

    config to fork sepolia

--- projet_4/hardhat/hardhat.config.ts
+++ projet_4/hardhat/hardhat.config.ts
@@ -1,7 +1,23 @@
 import { HardhatUserConfig } from "hardhat/config";
+require('dotenv').config();
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      forking: {
+        // @ts-ignore
+        url: process.env.SEPOLIA_RPC_URL
+      }
+    },
+    // kovan: {
+    //   url: process.env.KOVAN_RPC_URL,
+    //   accounts: {
+    //     mnemonic: process.env.MNEMONIC
+    //   }
+    // }
+  },
   solidity: "0.8.18",
 };
 
