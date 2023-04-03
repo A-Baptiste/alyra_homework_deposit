052d67d998b80ecfce10a4c9a2fe43db998f7015
Baptiste <avocalam@gmail.com>
2023-04-03 21:23:44 +0200

    update app

--- projet_4/client/src/App.tsx
+++ projet_4/client/src/App.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import NavBar from './components/NavBar'
+import Timer from './components/Timer'
+import OwnerButtons from './components/OwnerButtons';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,6 +9,8 @@ function App() {
   return (
     <div>
      <NavBar />
+     <Timer />
+     <OwnerButtons />
     </div>
   )
 }
