c5dbab1229b16ba73d29ba64069ee3fcdb66ac0a
Baptiste <avocalam@gmail.com>
2023-04-03 12:23:17 +0200

    setup rainbowkit and wagmi

--- projet_4/client/src/main.tsx
+++ projet_4/client/src/main.tsx
@@ -1,10 +1,42 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { WagmiConfig, createClient, configureChains } from 'wagmi';
+import { hardhat, goerli } from 'wagmi/chains';
+import { publicProvider } from 'wagmi/providers/public';
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+  lightTheme,
+} from '@rainbow-me/rainbowkit';
+import './index.css';
+import '@rainbow-me/rainbowkit/styles.css';
+import 'react-toastify/dist/ReactToastify.css';
+
+const { chains, provider } = configureChains(
+  [hardhat, goerli],
+  [publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'My RainbowKit App',
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: false,
+  connectors,
+  provider,
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <WagmiConfig client={wagmiClient}>
+    <RainbowKitProvider
+      chains={chains}
+      theme={lightTheme({
+        accentColor: '#7b3fe4',
+      })}
+    >
+      <App />
+    </RainbowKitProvider>
+  </WagmiConfig>
+);
