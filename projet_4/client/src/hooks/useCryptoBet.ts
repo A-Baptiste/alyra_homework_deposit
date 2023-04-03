5043a9a52ed699912068971dd1ac4edf781cd173
Baptiste <avocalam@gmail.com>
2023-04-03 21:24:07 +0200

    setup event listeners

--- projet_4/client/src/hooks/useCryptoBet.ts
+++ projet_4/client/src/hooks/useCryptoBet.ts
@@ -5,11 +5,15 @@ import artifact from '../contracts/CryptoBet.json';
 import { toast } from 'react-toastify';
 import { ethers } from 'ethers';
 
+const BET_VALUE = "0.000000000000000010" // 10 wei
+
 export function useCryptoBet() {
   const [userStatus, setUserStatus] = useState<string>("guest");
 
   const { data: signerData } = useSigner();
   const { address } = useAccount();
+
+    // -------------------------------------------------------- CONTRACT GETTERS
 
   const cryptoBet = useContract({
     address: import.meta.env.VITE_CRYPTOBET_ADDR,
@@ -23,9 +27,79 @@ export function useCryptoBet() {
     functionName: 'owner',
   });
 
+  // -------------------------------------------------------- EVENTS LISTENERS
+
+  useContractEvent({
+    address: import.meta.env.VITE_CRYPTOBET_ADDR,
+    abi: artifact.abi,
+    eventName: 'evt_nextRound',
+    listener(node, label, owner) {
+      console.log("evt_nextRound", node, label, owner);
+    },
+  });
+
+  useContractEvent({
+    address: import.meta.env.VITE_CRYPTOBET_ADDR,
+    abi: artifact.abi,
+    eventName: 'evt_betFinish',
+    listener(node, label, owner) {
+      console.log("evt_betFinish", node, label, owner);
+    },
+  });
+
+  // -------------------------------------------------------- EVENTS FETCHERS
+
+  async function getLastRound() {
+    if (!cryptoBet) return;
+
+    try {
+      const cryptoBetFilter = cryptoBet.filters.evt_nextRound();
+      if (!cryptoBetFilter) return;
+      const cryptoBetEvents = await cryptoBet.queryFilter(
+        cryptoBetFilter
+      );
+      if (!cryptoBetEvents) return;
+
+      cryptoBetEvents.map((elem) => {
+        //@ts-ignore
+        const roundTimestamp = elem.args.startedtAt.toNumber();
+        console.log('round time stamp ', roundTimestamp)
+        var roundDate = new Date(roundTimestamp);
+        console.log('round', roundDate);
+      })
+      // console.log("get rounds ", cryptoBetEvents);
+
+      // const fetchedVoters = voterRegisteredEvents.map(
+      //   (voter) => voter?.args?.voterAddress
+      // ) as string[];
+
+      // setVoters(fetchedVoters);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  // -------------------------------------------------------- FUNCTIONS
+
   const getUserStatus = async () => {
     if (votingOwner === address) {
       setUserStatus('owner');
+    }
+  };
+
+  const handleNextRound = async () => {
+    console.log('NEXT ROUND FORCE');
+    if (cryptoBet) {
+      await cryptoBet.nextRound(); 
+    }
+  };
+
+  const handleRegisterBet = async () => {
+    console.log('REGISTER BET (up)');
+    if (cryptoBet) {
+      await cryptoBet.registerBet(1, { value: ethers.utils.parseEther(BET_VALUE) });
+
+      // const result = cryptoBet.registerBet(2, { value: ethers.utils.parseEther(BET_VALUE) });
     }
   };
 
@@ -33,5 +107,5 @@ export function useCryptoBet() {
     getUserStatus();
   }, [address]);
 
-  return { address, userStatus }
+  return { address, userStatus, handleNextRound, handleRegisterBet, getLastRound }
 };
