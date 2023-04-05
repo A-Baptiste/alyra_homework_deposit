4d6d99bf70dd9f684e35b34e0e5e89de11e9f22e
Baptiste <avocalam@gmail.com>
2023-04-05 17:58:13 +0200

    make card last

--- projet_4/client/src/components/cards/CardLast.tsx
+++ projet_4/client/src/components/cards/CardLast.tsx
@@ -1,15 +1,50 @@
 import { useAccount } from 'wagmi';
 import { useCryptoBet } from '../../hooks/useCryptoBet';
+import { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
 
 interface Props {
   useToken: boolean;
 }
 
 function CardLast({ useToken }: Props) {
-  const { isConnected } = useAccount();
-  const { handleNextRound, handleRegisterBet, getLastRound } = useCryptoBet();
+  const { userBet, userResult, handleClaimReward } = useCryptoBet();
+  const [hasFinish, setHasFinish] = useState<string>("unknow");
 
-  const hasReward = true;
+  useEffect(() => {
+    if ( userBet && userBet.data && hasFinish !== "claimed" ) {
+      //@ts-ignore
+      if (userBet.data.betStatus == 3) {
+        setHasFinish("reward");
+      }
+      //@ts-ignore
+      if (userBet.data.betStatus == 2) {
+        setHasFinish("loose");
+      }
+    }
+  }, [userBet, userResult])
+
+  useEffect(() => {
+    if (userResult === "win") {
+      setHasFinish("reward");
+    }
+    if (userResult === "los") {
+      setHasFinish("loose");
+    }
+  }, [userResult])
+
+  const handleClaimBetFromCard = async () => {
+    const response = await handleClaimReward();
+    await response.wait();
+    console.log("claim finish")
+    if (hasFinish === "loose") {
+      toast.success("Pari terminé, rejouez !");
+    }
+    if (hasFinish === "reward") {
+      toast.success("Récompense distribuée !");
+    }
+    setHasFinish("claimed");
+  };
 
   return (
     <div className="card w-96 shadow-xl bg-[#2F2C2C]">
@@ -17,7 +52,7 @@ function CardLast({ useToken }: Props) {
         <h2 className="card-title">Session précedente</h2>
         <div className='card bg-base-100 h-full'>
           <div className="card-body flex flex-col gap-5 items-center justify-center">
-            <div className='mb-5'>Résultats</div>
+            <div>Résultats</div>
             <div className='flex gap-5'>
               <div className='flex flex-col items-center'>
                 <div className='text-sm'>Gagnants</div>
@@ -35,9 +70,20 @@ function CardLast({ useToken }: Props) {
             <div className='text-3xl text-primary font-bold'>
               $ 1 923.347
             </div>
-            {hasReward && (
-              <button className='btn btn-primary w-full mt-5'>
-                Réclamer vos gains
+            {hasFinish === "reward" && (
+              <button
+                className='btn btn-primary w-full mt-5'
+                onClick={handleClaimBetFromCard}
+              >
+                GAGNÉ: Réclamez vos gains
+              </button>
+            )}
+            {hasFinish === "loose" && (
+              <button
+                className='btn btn-error w-full mt-5'
+                onClick={handleClaimBetFromCard}
+              >
+                PERDU: Terminer le pari
               </button>
             )}
           </div>
