62577d62e536bfe9607cf39f51b556584a20e98b
Baptiste <avocalam@gmail.com>
2023-04-05 12:36:08 +0200

    temporary status display

--- projet_4/client/src/components/BetCards.tsx
+++ projet_4/client/src/components/BetCards.tsx
@@ -4,16 +4,29 @@ import CardLast from './cards/CardLast';
 import CardInfo from './cards/CardInfo';
 import CardBet from './cards/CardBet';
 
-function BetCards() {
+interface Props {
+  useToken: boolean;
+};
+
+function BetCards({ useToken }: Props) {
   const { isConnected } = useAccount();
   const { handleNextRound, handleRegisterBet, getLastRound } = useCryptoBet();
 
   return (
+  <>
+    <div>
+      {useToken ?
+        <div>Play with edft</div>
+        :
+        <div>Play with eth</div>
+      }
+    </div>
     <div className="flex justify-center gap-5 mb-5">
       <CardLast />
       <CardInfo />
       <CardBet />
     </div>
+  </>
   );
 }
 
