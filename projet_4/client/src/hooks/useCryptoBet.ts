12e378c441730f768fdd6e23be304287a927aeba
Baptiste <avocalam@gmail.com>
2023-04-05 15:57:32 +0200

    increment hook

--- projet_4/client/src/hooks/useCryptoBet.ts
+++ projet_4/client/src/hooks/useCryptoBet.ts
@@ -9,6 +9,7 @@ const BET_VALUE = "0.000000000000000010" // 10 wei
 
 export function useCryptoBet() {
   const [userStatus, setUserStatus] = useState<string>("guest");
+  const [bettersFromEvt, setBettersFromEvt] = useState<string[]>([]);
 
   const { data: signerData } = useSigner();
   const { address } = useAccount();
@@ -34,6 +35,13 @@ export function useCryptoBet() {
     args: [address],
   });
 
+  const userBet = useContractRead({
+    address: import.meta.env.VITE_CRYPTOBET_ADDR,
+    abi: artifact.abi,
+    functionName: 'getOneBetter',
+    args: [address],
+  });
+
   // -------------------------------------------------------- EVENTS LISTENERS
 
   useContractEvent({
@@ -51,6 +59,16 @@ export function useCryptoBet() {
     eventName: 'evt_betFinish',
     listener(node, label, owner) {
       console.log("evt_betFinish", node, label, owner);
+    },
+  });
+
+  useContractEvent({
+    address: import.meta.env.VITE_CRYPTOBET_ADDR,
+    abi: artifact.abi,
+    eventName: 'evt_newBet',
+    listener(node, label, owner) {
+      // console.log("evt_newBet", node, label, owner);
+      // getBetters();
     },
   });
 
@@ -86,6 +104,36 @@ export function useCryptoBet() {
     }
   }
 
+  async function getBetters() {
+    if (!cryptoBet) return;
+
+    try {
+      const cryptoBetFilter = cryptoBet.filters.evt_newBet();
+      if (!cryptoBetFilter) return;
+      const cryptoBetEvents = await cryptoBet.queryFilter(
+        cryptoBetFilter
+      );
+      if (!cryptoBetEvents) return;
+
+      console.log("betters", cryptoBetEvents);
+
+      let bettersArray = cryptoBetEvents.map((elem) => {
+        //@ts-ignore
+        return elem.args.userAddr;
+      })
+      setBettersFromEvt(bettersArray);
+      // console.log("rounds array ", bettersArray);
+
+      // const fetchedVoters = voterRegisteredEvents.map(
+      //   (voter) => voter?.args?.voterAddress
+      // ) as string[];
+
+      // setVoters(fetchedVoters);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   // -------------------------------------------------------- FUNCTIONS
 
   const getUserStatus = async () => {
@@ -104,25 +152,39 @@ export function useCryptoBet() {
     }
   };
 
-  const handleRegisterBet = async () => {
+  const handleRegisterBet = async (expectation: number, isToken: boolean) => {
     console.log('REGISTER BET (up)');
-    if (cryptoBet) {
-      await cryptoBet.registerBet(1, { value: ethers.utils.parseEther(BET_VALUE) });
-
-      // const result = cryptoBet.registerBet(2, { value: ethers.utils.parseEther(BET_VALUE) });
+    let response;
+    if ((expectation === 1 || expectation === 2) && cryptoBet) {
+      if (isToken) {
+        response = await cryptoBet.registerBetErc20(expectation);
+      } else {
+        response = await cryptoBet.registerBet(expectation, { value: ethers.utils.parseEther(BET_VALUE) });
+      }
     }
+    return response;
   };
 
+  // -------------------------------------------------------- FUNCTIONS
+
+  // trigger functions
   useEffect(() => {
     getUserStatus();
+  }, [address]);
+
+  // trigger event readers
+  useEffect(() => {
+    // getBetters();
   }, [address]);
 
   return {
     address,
     edftBalance,
     userStatus,
+    userBet,
     handleNextRound,
     handleRegisterBet,
-    getLastRound
+    getLastRound,
+    bettersFromEvt
   }
 };
