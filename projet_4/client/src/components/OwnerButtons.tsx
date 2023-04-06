97184a21da368c7a681a5c2ca16bb9b89d637024
Baptiste <avocalam@gmail.com>
2023-04-06 21:11:20 +0200

    style

--- projet_4/client/src/components/OwnerButtons.tsx
+++ projet_4/client/src/components/OwnerButtons.tsx
@@ -12,32 +12,34 @@ function OwnerButtons() {
         <div className='text-lg font-bold mt-3'>
           Espace propiétaire
         </div>
-        <span className="flex flex-col items-center">
-          <span>Balance du contrat</span>
-          <div>
-          <b className='text-primary'>
-            {contractBalance?.formatted}
-          </b>
-          &nbsp;ETH
+        <div className='flex justify-center items-center gap-5'>
+          <span className="flex flex-col items-center w-52">
+            <span>Balance du contrat</span>
+            <div>
+            <b className='text-primary'>
+              {contractBalance?.formatted}
+            </b>
+            &nbsp;ETH
+            </div>
+          </span>
+          <div className='flex flex-col w-52'>
+            <input
+              className='input input-bordered input-primary input-sm'
+              type="number"
+              placeholder='00,0 ETH'
+            />
+            <button
+              className="btn btn-primary btn-sm"
+              onClick={() => console.log("click")}
+            >Transférer</button>
           </div>
-        </span>
-        <div className='flex items-center gap-5'>
-          <input
-            className='input input-bordered input-primary input-sm'
-            type="number"
-            placeholder='00,0 ETH'
-          />
           <button
-            className="btn btn-primary btn-sm"
-            onClick={() => console.log("click")}
-          >Transférer</button>
+            className="btn btn-error btn-sm mb-3 w-52"
+            onClick={handleNextRound}
+          >
+            demo: force next round
+          </button>
         </div>
-        <button
-          className="btn btn-error btn-sm mb-3"
-          onClick={handleNextRound}
-        >
-          demo: force next round
-        </button>
       </>
       }
     </div>
