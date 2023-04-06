a2084eb5350a276fb79ad1edc30bdd8da83c1b49
Baptiste <avocalam@gmail.com>
2023-04-06 17:02:55 +0200

    patch navbar style

--- projet_4/client/src/components/NavBar.tsx
+++ projet_4/client/src/components/NavBar.tsx
@@ -6,17 +6,22 @@ function NavBar() {
   const { isConnected } = useAccount();
   const { userStatus } = useCryptoBet();
 
+  const getUserStatus = () => {
+    if (userStatus === "owner") { return "propriétaire"}
+    return "invité(e)";
+  };
+
   return (
     <div className="navbar flex justify-between bg-[#2F2C2C]">
-      <span className='text-primary font-bold text-2xl border px-3 py-1 border-primary rounded-full'>
-        EASYDAPP
-      </span>
+      <button className='btn font-bold text-2xl'>
+        easy<div className='text-primary'>dapp</div>
+      </button>
       <div className="flex flex-col items-end">
         <div className="flex flex-col items-center">
           <ConnectButton />
           {isConnected ? (
-            <span className='text-primary font-bold'>
-              Bonjour ! Vous êtes {userStatus} !
+            <span className='font-bold flex'>
+              Bonjour ! Vous êtes&nbsp;<div className='text-primary'>{getUserStatus()}</div>&nbsp;!
             </span>
           ) : (
             <span className='text-primary font-bold'>
